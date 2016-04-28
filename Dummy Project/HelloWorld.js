bd96e0cb7224ee352de110a07b0e6f5a98e09db1
vtomi86 <vtomi86@gmail.com>
2016-04-28 13:13:04 +0200

    Just another commit for branch 3

--- Dummy Project/HelloWorld.js
+++ Dummy Project/HelloWorld.js
@@ -9,6 +9,7 @@ var favcolor = window.prompt("What is your favorite color");
 favcolor = "Red";
 // Modification 2
 // Just a commit for branch 3
+// Just another commit for branch 3
 
 // write "Hello" followed by person' name to browser window
 document.write("Hello " + who);
