40362c78cf3d79198928d69cf00efd919025ee76
vtomi86 <vtomi86@gmail.com>
2016-04-28 12:27:49 +0200

    DUmmy commit 2

---
+++ Dummy Project/HelloWorld.js
@@ -0,0 +1,16 @@
+// Hello in Javascript
+// display prompt box that ask for name and 
+// store result in a variable called who
+var who = window.prompt("What is your name");
+
+// display prompt box that ask for favorite color and 
+// store result in a variable called favcolor
+var favcolor = window.prompt("What is your favorite color");
+favcolor = "Red";
+// Modification
+
+// write "Hello" followed by person' name to browser window
+document.write("Hello " + who);
+
+// Change background color to their favorite color
+document.bgColor = favcolor;
